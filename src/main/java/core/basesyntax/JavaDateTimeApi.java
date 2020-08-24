561ebe45555642342b3715a25befb0d252b4290a
FYarkiy <flx_jb@yahoo.com>
2020-08-24 14:56:21 +0300

    Date and Time conversion using DateTime API

--- src/main/java/core/basesyntax/JavaDateTimeApi.java
+++ src/main/java/core/basesyntax/JavaDateTimeApi.java
@@ -1,12 +1,30 @@
 package core.basesyntax;
 
+import java.time.DateTimeException;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 
 public class JavaDateTimeApi {
+
+    public static final DateTimeFormatter DATE_FORMATTER_ENGLISH
+            = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
+    public static final DateTimeFormatter DATE_FORMATTER_YYYY_MM_DD
+            = DateTimeFormatter.ofPattern("yyyy MM dd");
+    public static final DateTimeFormatter FULL_DATE_TIME_FORMATTER_ENGLISH
+            = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm", Locale.ENGLISH);
+    public static final ZoneOffset UKRAINE_ZONE_OFFSET = ZoneOffset.of("+02:00");
+    public LocalDate localDateNow = LocalDate.now();
+
     /**
      * Верните текущую дату в виде строки в зависимости от запроса.
      *
@@ -19,7 +37,16 @@ public class JavaDateTimeApi {
      *                 В любом другом случае бросить DateTimeException
      **/
     public String todayDate(DateTimePart datePart) {
-        return "Today";
+        Map<DateTimePart, String> dateRequest = new HashMap<>();
+        dateRequest.put(DateTimePart.FULL, localDateNow.toString());
+        dateRequest.put(DateTimePart.YEAR, String.valueOf(localDateNow.getYear()));
+        dateRequest.put(DateTimePart.MONTH, String.valueOf(localDateNow.getMonth()));
+        dateRequest.put(DateTimePart.DAY, String.valueOf(localDateNow.getDayOfYear()));
+        try {
+            return dateRequest.get(datePart);
+        } catch (DateTimeException e) {
+            throw new DateTimeException("Incorrect date / date format");
+        }
     }
 
     /**
@@ -31,7 +58,11 @@ public class JavaDateTimeApi {
      *                   - 3-й элемент массива - день (число);
      */
     public Optional<LocalDate> getDate(Integer[] dateParams) {
-        return Optional.empty();
+        try {
+            return Optional.of(LocalDate.of(dateParams[0], dateParams[1], dateParams[2]));
+        } catch (DateTimeException | ArrayIndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
     }
 
     /**
@@ -39,7 +70,7 @@ public class JavaDateTimeApi {
      * Верните измененное время на указаную величину.
      */
     public LocalTime addHours(LocalTime localTime, Integer hoursToAdd) {
-        return LocalTime.now();
+        return localTime.plusHours(hoursToAdd);
     }
 
     /**
@@ -47,7 +78,7 @@ public class JavaDateTimeApi {
      * Верните измененное время на указаную величину.
      */
     public LocalTime addMinutes(LocalTime localTime, Integer minutesToAdd) {
-        return LocalTime.now();
+        return localTime.plusMinutes(minutesToAdd);
     }
 
     /**
@@ -55,7 +86,7 @@ public class JavaDateTimeApi {
      * Верните измененное время на указаную величину.
      */
     public LocalTime addSeconds(LocalTime localTime, Integer secondsToAdd) {
-        return LocalTime.now();
+        return localTime.plusSeconds(secondsToAdd);
     }
 
     /**
@@ -63,7 +94,7 @@ public class JavaDateTimeApi {
      * Верните получившуюся дату
      */
     public LocalDate addWeeks(LocalDate localDate, Integer numberOfWeeks) {
-        return LocalDate.now();
+        return localDate.plusWeeks(numberOfWeeks);
     }
 
     /**
@@ -74,16 +105,19 @@ public class JavaDateTimeApi {
      * - "someDate is today" - если someDate - сегодня
      */
     public String beforeOrAfter(LocalDate someDate) {
-        return someDate + "is today";
+        return someDate.isEqual(localDateNow) ? someDate + " is today"
+                : someDate.isBefore(localDateNow) ? someDate + " is before " + localDateNow
+                : someDate + " is after " + localDateNow;
     }
 
     /**
      * Дана дата в строковом формате и временная зона.
      * Верните LocalDateTime в этой временной зоне.
+     *
      * @return LocalDateTime
      */
     public LocalDateTime getDateInSpecificTimeZone(String dateInString, String zone) {
-        return LocalDateTime.now();
+        return LocalDateTime.ofInstant(Instant.parse(dateInString), ZoneId.of(zone));
     }
 
     /**
@@ -96,7 +130,7 @@ public class JavaDateTimeApi {
      * OffsetDateTime советуют использовать при записи даты в базу данных.
      */
     public OffsetDateTime offsetDateTime(LocalDateTime localTime) {
-        return OffsetDateTime.now();
+        return OffsetDateTime.of(localTime, UKRAINE_ZONE_OFFSET);
     }
 
     /**
@@ -104,7 +138,11 @@ public class JavaDateTimeApi {
      * Необходимо вернуть Optional даты в LocalDate формате
      */
     public Optional<LocalDate> parseDate(String date) {
-        return Optional.empty();
+        try {
+            return Optional.of(LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE));
+        } catch (DateTimeException e) {
+            return Optional.empty();
+        }
     }
 
     /**
@@ -112,7 +150,12 @@ public class JavaDateTimeApi {
      * Необходимо вернуть Optional даты в LocalDate формате
      */
     public Optional<LocalDate> customParseDate(String date) {
-        return Optional.empty();
+        try {
+            return Optional.of(LocalDate.parse(LocalDate.parse(date, DATE_FORMATTER_ENGLISH)
+                    .format(DATE_FORMATTER_YYYY_MM_DD), DATE_FORMATTER_YYYY_MM_DD));
+        } catch (DateTimeException e) {
+            return Optional.empty();
+        }
     }
 
     /**
@@ -122,6 +165,6 @@ public class JavaDateTimeApi {
      * например: "01 January 2000 18:00",
      */
     public String formatDate(LocalDateTime dateTime) {
-        return "";
+        return dateTime.format(FULL_DATE_TIME_FORMATTER_ENGLISH);
     }
 }
