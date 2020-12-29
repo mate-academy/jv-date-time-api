f8c622d4d34707648bb1c1307d5c007c460db4ce
AndreiYazadzhi <belator251000@gmail.com>
2020-12-29 16:41:33 +0200

    Add solution

--- src/main/java/core/basesyntax/JavaDateTimeApi.java
+++ src/main/java/core/basesyntax/JavaDateTimeApi.java
@@ -1,116 +1,98 @@
 package core.basesyntax;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Optional;
 
 public class JavaDateTimeApi {
-    /**
-     * Return the current date as a String depending on a query.
-     *
-     * The query can be passed for the whole date or for it's part:
-     *      - FULL - current date as a whole: year, month, day of month
-     *        formatted as `YYYY-MM-DD` (a default return value);
-     *      - YEAR - current year;
-     *      - MONTH - name of the current month;
-     *      - DAY - current day of month;
-     * In any other case throw DateTimeException.
-     **/
+    private static final int NUMBER_OF_DAYS = 31;
+    private static final int NUMBER_OF_MONTH = 12;
+
     public String todayDate(DateTimePart datePart) {
-        return "Today";
+        switch (datePart) {
+            case FULL:
+                return String.valueOf(LocalDate.now());
+            case YEAR:
+                return String.valueOf(LocalDate.now().getYear());
+            case MONTH:
+                return String.valueOf(LocalDate.now().getMonth());
+            case DAY:
+                return String.valueOf(LocalDate.now().getDayOfMonth());
+            default: throw new DateTimeException("No such option");
+        }
     }
 
-    /**
-     * Given an Array of 3 elements, where
-     *         - 1-st element is a `year`;
-     *         - 2-nd element is s `month`;
-     *         - 3-rd element is a `day of month`;
-     *
-     * Return Optional of a date built from these elements.
-     */
     public Optional<LocalDate> getDate(Integer[] dateParams) {
-        return Optional.empty();
+        if (dateParams.length == 0
+                || dateParams[1] > NUMBER_OF_MONTH
+                || dateParams[2] > NUMBER_OF_DAYS) {
+            return Optional.empty();
+        }
+        return Optional.of(LocalDate.of(dateParams[0], dateParams[1], dateParams[2]));
     }
 
-    /**
-     * Given the time and the number of hours to add, return the changed time.
-     */
     public LocalTime addHours(LocalTime localTime, Integer hoursToAdd) {
-        return LocalTime.now();
+        return localTime.plusHours(hoursToAdd);
     }
 
-    /**
-     * Given the time and the number of minutes to add, return the changed time.
-     */
     public LocalTime addMinutes(LocalTime localTime, Integer minutesToAdd) {
-        return LocalTime.now();
+        return localTime.plusMinutes(minutesToAdd);
     }
 
-    /**
-     * Given the time and the number of seconds to add, return the changed time.
-     */
     public LocalTime addSeconds(LocalTime localTime, Integer secondsToAdd) {
-        return LocalTime.now();
+
+        return localTime.plusSeconds(secondsToAdd);
     }
 
-    /**
-     * Given the date and the number of weeks to add, return the changed date.
-     */
     public LocalDate addWeeks(LocalDate localDate, Integer numberOfWeeks) {
-        return LocalDate.now();
+
+        return localDate.plusWeeks(numberOfWeeks);
     }
 
-    /**
-     * Given a random `someDate` date, return one of the following Strings:
-     *     - "`someDate` is after `currentDate`"
-     *                  if `someDate` is in the future relating to the `current date`;
-     *     - "`someDate` is before `currentDate`"
-     *                  if `someDate` is in the past relating to the `current date`;
-     *     - "`someDate` is today"
-     *                  if `someDate` is today;
-     */
     public String beforeOrAfter(LocalDate someDate) {
-        return someDate + "is today";
+        LocalDate currentDate = LocalDate.now();
+        if (someDate.isBefore(currentDate)) {
+            return someDate + " is before " + currentDate;
+        }
+        if (someDate.isAfter(currentDate)) {
+            return someDate + " is after " + currentDate;
+        }
+        return someDate + " is today";
     }
 
-    /**
-     * Given a String representation of a date and some timezone,
-     * return LocalDateTime in this timezone.
-     */
     public LocalDateTime getDateInSpecificTimeZone(String dateInString, String zone) {
-        return LocalDateTime.now();
+        return ZonedDateTime.parse(dateInString)
+                .withZoneSameInstant(ZoneId.of(zone)).toLocalDateTime();
     }
 
-    /**
-     * Given some LocalDateTime, return an OffsetDateTime with the local time offset applied
-     * (`+02:00` for Ukraine).
-     *
-     * Example: we receive a LocalDateTime with a value `2019-09-06T13:17`.
-     *          We should return the OffsetDateTime with a value `2019-09-06T13:17+02:00`,
-     *          where `+02:00` is the offset for our local timezone.
-     *
-     * OffsetDateTime is recommended to use for storing date values in a database.
-     */
     public OffsetDateTime offsetDateTime(LocalDateTime localTime) {
-        return OffsetDateTime.now();
+        return localTime.atOffset(ZonedDateTime.now().getOffset());
     }
 
-    /**
-     * Given a String formatted as `yyyymmdd`,
-     * return Optional of this date as a LocalDate.
-     */
     public Optional<LocalDate> parseDate(String date) {
-        return Optional.empty();
+        if (Integer.parseInt(date.substring(6)) > NUMBER_OF_DAYS
+                || Integer.parseInt(date.substring(4, 6)) > NUMBER_OF_MONTH) {
+            return Optional.empty();
+        }
+        return Optional.of(LocalDate.parse(date,
+                DateTimeFormatter.ofPattern("yyyyMMdd")));
     }
 
-    /**
-     * Given a String formatted as `d MMM yyyy` (MMM - Sep, Oct, etc),
-     * return Optional of this date as a LocalDate.
-     */
     public Optional<LocalDate> customParseDate(String date) {
-        return Optional.empty();
+        if (date.isEmpty()
+                || date.isBlank()
+                || Integer.parseInt(date.substring(0, 2)) > NUMBER_OF_DAYS) {
+            return Optional.empty();
+        }
+        return Optional.of(LocalDate.parse(date,
+                DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.US)));
     }
 
     /**
@@ -120,6 +102,6 @@ public class JavaDateTimeApi {
      * Example: "01 January 2000 18:00".
      */
     public String formatDate(LocalDateTime dateTime) {
-        return "";
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm", Locale.US));
     }
 }
